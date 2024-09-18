35db76dee2ef21eb68cc4f8eee52fd0eab284bb0
arthurxzl <juanarthur96@gmail.com>
2024-09-17 23:56:14 -0300

    telas visualizar e java

--- app/src/main/java/com/example/maratona/Tela_concluir.java
+++ app/src/main/java/com/example/maratona/Tela_concluir.java
@@ -1,6 +1,9 @@
 package com.example.maratona;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,11 +17,21 @@ public class Tela_concluir extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_tela_concluir);
+        setContentView(R.layout.activity_participar_tela);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button btnConcluir = findViewById(R.id.btnParticipar);
+        btnConcluir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Tela_concluir.this, visualizarConcluidas.class);
+                startActivity(intent);
+            }
         });
     }
 }
